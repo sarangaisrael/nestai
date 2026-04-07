e0dfa1636c1d0c76627b9d70261d50151e97fc67
sarangaisrael <sarangaisrael@gmail.com>
2026-04-07 13:49:07 +0300

    עדכון Capacitor לדומיין החדש

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.7d309298db8d466697f9a97ef81286a0',
-  appName: 'logme',
+  appId: 'care.nestai.app',
+  appName: 'NestAI',
   webDir: 'dist',
   server: {
-    url: 'https://7d309298-db8d-4666-97f9-a97ef81286a0.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
+    url: 'https://www.nestai.care',
+    cleartext: false,
   },
 };
 
